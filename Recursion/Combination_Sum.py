a6deb79ce62088a63a17b242e5b467afc619a83f
szzd7223 <mohdsaadshaikh17@gmail.com>
2025-05-02 23:19:35 +0530

    Combination_Sum

---
+++ Recursion/Combination_Sum.py
@@ -0,0 +1,26 @@
+def combinationSum(candidates, target):
+    res = []
+
+    def dfs(i, cur, total):
+        if total == target:
+            res.append(cur.copy())
+            return
+        
+        if i >= len(candidates) or total > target:
+            return
+        
+        cur.append(candidates[i])
+        dfs(i, cur, total + candidates[i])
+
+        cur.pop()
+        dfs(i+1, cur, total)
+
+    dfs(0, [], 0)
+
+    return res
+
+
+candidates = [2,3,6,7]
+target = 7
+
+print(combinationSum(candidates, target))
