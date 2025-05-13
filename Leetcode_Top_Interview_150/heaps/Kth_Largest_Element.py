5795fd249bd261b4d55bb08484fb91bba7cd0158
szzd7223 <mohdsaadshaikh17@gmail.com>
2025-05-13 22:53:41 +0530

    Kth_largest_element_(heaps)

---
+++ Leetcode_Top_Interview_150/heaps/Kth_Largest_Element.py
@@ -0,0 +1,11 @@
+import heapq
+def KthLargest(arr, k):
+    min_heap = []
+
+    for num in arr:
+        heapq.heappush(min_heap, num)
+
+        if len(min_heap) > k:
+            heapq.heappop(min_heap) # This will ensure, that the size of heap is k (Which is top K largest elements)
+
+    return min_heap[0] # The first element of final min heap will me Kth Largest because it is the smallest among top K largest number (Please make sense of it)
